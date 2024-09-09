7aa3887dc5db504a16374b2938cd94884a4e2eec
samuelscheit <github@samuelscheit.com>
2024-09-09 22:47:58 +0200

    feat: rust impl

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,27 @@
-fn main() -> Result<(), markdown::message::Message> {
-    println!(
-        "{:?}",
-        markdown::to_mdast("# Hey, *you*!", &markdown::ParseOptions::default())?
-    );
+use std::fs;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = std::env::args()
+        .nth(1)
+        .expect("Please provide a path to a markdown file");
+    let path = std::path::Path::new(&path);
+
+    let file = fs::read_to_string(path)?;
+
+    let start = std::time::Instant::now();
+
+    for _ in 0..1000 {
+        // let node = markdown::to_mdast(&file, &markdown::ParseOptions::default());
+
+        let parser = pulldown_cmark::Parser::new(&file);
+
+        let mut html_output = String::new();
+        pulldown_cmark::html::push_html(&mut html_output, parser);
+
+        println!("{}", html_output.len());
+    }
+
+    println!("Time: {:?}", start.elapsed());
 
     Ok(())
 }
